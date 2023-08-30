77e57bb92f143be6e6e4e05084a0445fc10d9419
Selim Nahimi <selimnahimi@gmail.com>
2023-08-30 13:09:49 +0200

    Added loreipsum

--- src/com/gfa/memorymanagement/unoptimized/P05Caching/LoremIpsum.java
+++ src/com/gfa/memorymanagement/unoptimized/P05Caching/LoremIpsum.java
@@ -10,46 +10,28 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class LoremIpsum {
   public static void main(String[] args) throws Exception {
-    optimized();
-  }
-
-  private static void optimized() throws FileNotFoundException {
-    File file = new File("assets/loremipsum.txt");
-    ArrayList<Integer> textIndices = new ArrayList<>();
-
-    DB db = DBMaker.fileDB("words.db").make();
-    HTreeMap wordMap = db.hashMap("loremIpsum").createOrOpen();
-    Scanner scanner = new Scanner(file);
-
-    int counter = 0;
-    while (scanner.hasNext()) {
-      String word = scanner.next();
-      if (!wordMap.containsKey(word)) {
-        wordMap.put(word, counter++);
-      }
-
-      int index = (int) wordMap.get(word);
-
-      textIndices.add(index);
-    }
-
-    System.out.println(textIndices);
-
-    for (int index : textIndices) {
-      System.out.print(wordMap.get(index) + " ");
-    }
-    System.out.println(wordMap.size());
-
-    db.close();
+    unoptimized();
   }
 
   private static void unoptimized() throws IOException {
     String loremIpsum = Files.readAllLines(Path.of("assets/loremipsum.txt")).stream()
             .reduce((out, elem) -> out + " " + elem)
             .get();
+
+    HashMap<String, Integer> wordMap = new HashMap<>();
+
+    for (String word : loremIpsum.split(" ")) {
+      if (!wordMap.containsKey(word)) {
+        wordMap.put(word, 1);
+      }
+
+      int count = wordMap.get(word);
+      wordMap.put(word, count + 1);
+    }
   }
 }
