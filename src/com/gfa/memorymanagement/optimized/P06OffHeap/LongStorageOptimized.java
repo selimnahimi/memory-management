59e2bfa67ca7b1409086d2fe22a6b46c2bf78059
Selim Nahimi <selimnahimi@gmail.com>
2023-08-31 09:16:42 +0200

    Update LongStorageOptimized.java

--- src/com/gfa/memorymanagement/optimized/P06OffHeap/LongStorageOptimized.java
+++ src/com/gfa/memorymanagement/optimized/P06OffHeap/LongStorageOptimized.java
@@ -3,10 +3,30 @@ package com.gfa.memorymanagement.optimized.P06OffHeap;
 import net.openhft.chronicle.set.ChronicleSet;
 import net.openhft.chronicle.set.ChronicleSetBuilder;
 
-import java.time.Instant;
-import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
+
+/*
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+To run this, you need to change some JVM settings.
+Add these VM parameters:
+--add-exports=java.base/jdk.internal.ref=ALL-UNNAMED
+--add-exports=java.base/sun.nio.ch=ALL-UNNAMED
+--add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED
+--add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED
+--add-opens=jdk.compiler/com.sun.tools.javac=ALL-UNNAMED
+--add-opens=java.base/java.lang=ALL-UNNAMED
+--add-opens=java.base/java.lang.reflect=ALL-UNNAMED
+--add-opens=java.base/java.io=ALL-UNNAMED
+--add-opens=java.base/java.util=ALL-UNNAMED
+
+In IntelliJ IDEA, go to "Edit Configurations" (top right),
+then inside click on "Modify Options" (small blue link on the right),
+and choose "Add VM options".
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*/
 
 public class LongStorageOptimized {
   public static void main(String[] args) throws Exception {
@@ -17,7 +37,9 @@ public class LongStorageOptimized {
     ChronicleSet<Long> set = ChronicleSetBuilder.of(Long.class)
             .entries(30_000_000)
             .create();
-    return sumNumbers(set);
+    long result = sumNumbers(set);
+    set.close(); // free off-heap memory
+    return result;
   }
 
   private static long sumNumbers(Set<Long> numbers) throws InterruptedException {
